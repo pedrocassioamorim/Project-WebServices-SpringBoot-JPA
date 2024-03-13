b976b6d6afb4cdfda046bb6defd8a7294d4213a0
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-13 18:25:44 -0300

    implementando Repositorio JPA, injecao de dependencia e database seeding

---
+++ src/main/java/com/potiratech/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.potiratech.course.repositories;
+
+import com.potiratech.course.entites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
