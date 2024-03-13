e7ab4b959d582e4597877536c4b3a01749f23057
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-13 18:11:47 -0300

    implementando banco de dados H2, test profile e JPA

--- src/main/java/com/potiratech/course/entites/User.java
+++ src/main/java/com/potiratech/course/entites/User.java
@@ -1,12 +1,20 @@
 package com.potiratech.course.entites;
 
+
+
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -25,8 +33,6 @@ public class User implements Serializable {
     }
 
     // Getters and Setters:
-
-
     public Long getId() {
         return id;
     }
