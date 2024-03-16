56a1e19fa71512f1745a248b55e423f3701c638f
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-15 21:40:25 -0300

    salvando pequenas alterações

--- src/main/java/com/potiratech/course/repositories/UserRepository.java
+++ src/main/java/com/potiratech/course/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.potiratech.course.repositories;
 import com.potiratech.course.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
---
+++ src/main/java/com/potiratech/course/services/UserService.java
@@ -0,0 +1,19 @@
+package com.potiratech.course.services;
+
+import com.potiratech.course.entites.User;
+import com.potiratech.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+}
