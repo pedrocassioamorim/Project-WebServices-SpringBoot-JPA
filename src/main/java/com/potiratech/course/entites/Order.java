e0dccbcb85e31392c1d647439032f213c31e0882
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-16 22:22:30 -0300

    entidade pedido, parte 1

---
+++ src/main/java/com/potiratech/course/entites/Order.java
@@ -0,0 +1,78 @@
+package com.potiratech.course.entites;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long Id;
+
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+
+    public Order(Long id, Instant moment, User client) {
+        this.Id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Order(){}
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(Id, order.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "Id=" + Id +
+                ", moment=" + moment +
+                ", client=" + client +
+                '}';
+    }
+}
