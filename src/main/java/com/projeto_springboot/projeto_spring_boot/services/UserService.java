ea250af975d8ab0253b42ed8cc0a5c88d804d07d
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-19 16:30:07 -0300

    implementando exceções de dabase e delete

--- src/main/java/com/projeto_springboot/projeto_spring_boot/services/UserService.java
+++ src/main/java/com/projeto_springboot/projeto_spring_boot/services/UserService.java
@@ -2,8 +2,11 @@ package com.projeto_springboot.projeto_spring_boot.services;
 
 import com.projeto_springboot.projeto_spring_boot.entites.User;
 import com.projeto_springboot.projeto_spring_boot.repositories.UserRepository;
+import com.projeto_springboot.projeto_spring_boot.services.exceptions.DatabaseException;
 import com.projeto_springboot.projeto_spring_boot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,8 +30,16 @@ public class UserService {
         return repository.save(obj);
     }
 
+
     public void delete(Long Id){
-        repository.deleteById(Id);
+        try {
+            repository.deleteById(Id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(Id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long Id, User obj){
